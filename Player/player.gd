b45daaee151ca6efc0c7a81159d45533659ee162
Nicolas <nicolas.programador@gmail.com>
2024-12-27 19:53:49 -0300

    Se limpia codigo

--- Player/player.gd
+++ Player/player.gd
@@ -6,8 +6,6 @@ func _physics_process(delta: float):
 	move()
 
 func move():
-	#Vector con los 4 mapas de entradas
-	#var direction = Input.get_vector("left","right","up","down").normalized()
-	#velocity = direction * speed
+	#Vector con los 4 mapas de entradas	
 	velocity = Input.get_vector("left","right","up","down") * speed
 	move_and_slide()
