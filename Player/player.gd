81ce32e0323e40817a95866d387b036fa73ea428
Nicolas <nicolas.programador@gmail.com>
2025-01-08 03:00:40 -0300

    Movimiento a la derecha

--- Player/player.gd
+++ Player/player.gd
@@ -1,11 +1,22 @@
 extends CharacterBody2D
 
+##Para iniciar algo antes del que juego inicie
+@onready var anims: AnimationPlayer = $AnimationPlayer
+
+
 var speed = 100
 
 func _physics_process(delta: float):
 	move()
+	animCtrl()
 
 func move():
 	#Vector con los 4 mapas de entradas	
 	velocity = Input.get_vector("left","right","up","down") * speed
 	move_and_slide()
+
+func animCtrl():
+	if velocity.x > 0:
+			anims.play("walkR")
+
+	
