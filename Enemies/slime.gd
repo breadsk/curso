ec87d6e26b9f6e52df0dc360d374328ac1e1c46a
Nicolas <nicolas.programador@gmail.com>
2025-01-14 22:16:28 -0300

    Slime creado con animaciones, coalisiones y movimiento con Marker2D

--- Enemies/slime.gd
+++ Enemies/slime.gd
@@ -1,1 +1,27 @@
 extends CharacterBody2D
+
+@export var endPoint : Marker2D
+
+var speed = 70
+var startPosition: Vector2
+var endPosition: Vector2
+var limit = 0.5
+
+func _ready():
+	startPosition = position
+	endPosition = endPoint.global_position
+	
+func _physics_process(delta: float):
+	move(delta)
+	
+func changeDirection():
+	var tempEnd = endPosition
+	endPosition = startPosition
+	startPosition = tempEnd
+
+func move(delta: float):
+	var moveDirection = (endPosition - position)
+	if moveDirection.length() < limit:
+		changeDirection()
+	velocity = moveDirection.normalized() * speed
+	move_and_slide()
