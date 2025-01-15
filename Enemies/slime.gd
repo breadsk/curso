0af63879d3067e934f1d9ab37c03058af7fa9d12
Nicolas <nicolas.programador@gmail.com>
2025-01-15 17:06:58 -0300

    Slime con aninaciones listo, con marker2D y con animacion

--- Enemies/slime.gd
+++ Enemies/slime.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
 @export var endPoint : Marker2D
+@onready var slime: Sprite2D = $Slime
+@onready var anims: AnimationPlayer = $AnimationPlayer
+
 
 var speed = 70
 var startPosition: Vector2
 var endPosition: Vector2
 var limit = 0.5
+var lastDir = "D"
 
 func _ready():
 	startPosition = position
@@ -13,6 +17,7 @@ func _ready():
 	
 func _physics_process(delta: float):
 	move(delta)
+	animCtrl()
 	
 func changeDirection():
 	var tempEnd = endPosition
@@ -25,3 +30,22 @@ func move(delta: float):
 		changeDirection()
 	velocity = moveDirection.normalized() * speed
 	move_and_slide()
+
+func animCtrl():
+	##La primera animación es la predefinida
+	if velocity.x > 0:
+		slime.flip_h = false;
+		anims.play("walkR")
+		lastDir = "R"
+	elif velocity.x < 0:
+		slime.flip_h = true;
+		anims.play("walkR")
+		lastDir = "R"
+	elif velocity.y > 0:
+		anims.play("walkD")	
+		lastDir = "D"
+	elif velocity.y < 0:
+		anims.play("walkU")
+		lastDir = "U"
+	else:
+		anims.play("idle"+lastDir)#Lo concatena
